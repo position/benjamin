5b9f714c38c0de98b29228fef2078a409e812690
position <positionfov@gmail.com>
2018-12-24 16:49:58 +0900

    각 컴포넌트 생성

--- Benjamin/src/app/app.module.ts
+++ Benjamin/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { SceneComponent } from './scene/scene.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';

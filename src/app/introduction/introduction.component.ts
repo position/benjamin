b135881eb7524f763829db6c73205d7a5b069a9f
position <positionfov@gmail.com>
2018-12-27 18:43:04 +0900

    doc file update

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
     selector: 'benjamin-introduction',
     templateUrl: './introduction.component.html'
 })
 export class IntroductionComponent implements OnInit {
-
+    public imgPath: string = environment.assetsPath;
+    
     constructor() { }
 
     ngOnInit() {

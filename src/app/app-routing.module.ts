56bf9d66eb66c5593d6e467a858451dda71d0ed0
benjamin choi <positionfov@gmail.com>
2019-01-02 01:03:10 +0900

    app-routing file update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabled', preloadingStrategy: PreloadAllModules })],
+    imports: [RouterModule.forRoot(routes, { useHash: true, initialNavigation: 'enabled', preloadingStrategy: PreloadAllModules })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }

c8098e6723ad63422963258d81e27dc5e635db95
position <positionfov@gmail.com>
2019-02-28 12:13:58 +0900

    test space

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
     //{ path: '**', component: PageNotFoundComponent }
 ];
 
+
 @NgModule({
     imports: [RouterModule.forRoot(routes, { useHash: true, initialNavigation: 'enabled', preloadingStrategy: PreloadAllModules })],
     exports: [RouterModule]

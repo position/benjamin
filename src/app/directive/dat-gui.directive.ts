7d4434c7a936a56b9f849f5af184e0fda09bb9d7
position <positionfov@gmail.com>
2019-07-05 16:23:15 +0900

    test commit

--- src/app/directive/dat-gui.directive.ts
+++ src/app/directive/dat-gui.directive.ts
@@ -21,20 +21,20 @@ export class DatGuiDirective implements OnInit, OnDestroy{
                     this.camera.position.x = this.cameraPosition.x;
                     this.camera.position.y = this.cameraPosition.y;
                     this.camera.position.z = this.cameraPosition.z;
-                }   
+                }
             };
             let cam = this.gui.addFolder('Camera');
             cam.add(this.camera.position, 'x', -200, 200, 1).listen();
             cam.add(this.camera.position, 'y', -200, 200, 1).listen();
             cam.add(this.camera.position, 'z', -200, 200, 1).listen();
             cam.add(this.camera, 'fov', 1, 150).listen();
-            
+
             cam.open();
 
             this.gui.add(options, 'reset');
         }
     }
-
+    //test
     ngOnDestroy(){
         if(this.gui != null){
             this.gui.destroy();
